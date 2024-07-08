feeba6862a40f6e40bed2f11e773bd6ec4eb14df
vinay1945 <vinaykumarporandla19@gmail.com>
2024-07-08 12:08:51 +0530

    add

---
+++ 10_Practice_Set/Array_set.js
@@ -0,0 +1,9 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let sum;
+
+for (let i = 0; i <= arr.length; i++) {
+  sum = arr[i] + sum;
+  //   console.log(sum);
+}
+
+console.log(sum);
---
+++ 12_Piyush_Js/1_function.js
@@ -0,0 +1,11 @@
+// function muti(a, b){
+//     // console.log(a*b);
+//     return a * b
+// }
+
+// console.log(muti(5,5))
+
+
+function num(...num){
+    
+}
